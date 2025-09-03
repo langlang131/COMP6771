59f8f6023b51b04974006a94453ae2f182a70df2
Weimu Zhu <z5519251@ad.unsw.edu.au>
2025-09-03 13:46:59 +1000

    add number

--- exercises-w1/src/1.1/add_numbers.cpp
+++ exercises-w1/src/1.1/add_numbers.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
 int main(void) {
-    return 0;
-}
+	auto a = 0;
+	auto b = 0;
+
+	std::cout << "Please enter two numbers:";
+
+	if (!(std::cin >> a) || !(std::cin >> b)) {
+		std::cerr << "Something went wrong while reading an integer, bailing...\n";
+		return -1;
+	}
+	auto sum = a + b;
+	std::cout << sum << "\n";
+	return 0;
+};
